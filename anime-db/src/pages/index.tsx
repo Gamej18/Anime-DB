e142f17b8a5ea534ca8298a2c2c69a21f525a6a4
Gamej18 <jalenbowens@ymail.com>
2023-03-19 23:06:48 -0500

    new changes

--- anime-db/src/pages/index.tsx
+++ anime-db/src/pages/index.tsx
@@ -2,15 +2,24 @@ import Head from 'next/head';
 import { connectToDatabase } from "../../lib/mongodb";
 
 
-export default function HomePage({properties}: {properties: string}) {
+export default function HomePage({properties}: {properties: any}) {
   
   console.log(properties)
   return (
     <div>
-      Welcome!
+      <div>
+        {properties && properties.map((property: any) => (
+            <div>
+              {property.animetitle}
+            </div>
+          )
+        )}
+      </div>
+      <Head>
         <title>Creat Next App</title>
         <link rel="icon" href="/favicon,ico" />
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
     </div>
   );
 }
@@ -22,8 +31,15 @@ export async function getServerSideProps() {
   const data = await db.collection("animes").find({}).limit(5).toArray();
 
   const properties = JSON.parse(JSON.stringify(data));
+
+  const filtered = properties.map((property: { _id: any; animetitle: any; }) => {
+    return {
+      _id: property._id,
+      animetitle: property.animetitle
+    }
+  })
   
   return {
-    props: { properties: properties },
+    props: { properties: filtered },
   }
 }
