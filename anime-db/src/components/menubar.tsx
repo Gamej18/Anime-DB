80ba9ec7c5c6644abbe3100a64d18265a71293e3
Gamej18 <jalenbowens@ymail.com>
2023-03-28 02:13:27 -0500

    new changes

--- anime-db/src/components/menubar.tsx
+++ anime-db/src/components/menubar.tsx
@@ -2,16 +2,15 @@ import { Search } from "react-bootstrap-icons"
 
 export default function MenuBar() {
     return (
-        <div className="Menu">
-            <>
-                <Search size={20} />
-                <ul>
-                    <li>Genre</li>
-                    <li>Seasons</li>
-                    <li>Search</li>
-                    <li>Recommendations</li>
-                </ul>
-            </>
+        <div className="menu">
+            <Search size={20} className="item-search" />
+            <ul className="item-list">
+                <li className="item-genre">Genre</li>
+                <li className="item-season">Seasons</li>
+                <li className="item-searchs">Search</li>
+                <li className="item-reccomend">Recommendations</li>
+            </ul>
+            {/* <a href = "mailto: jalenbowenswork@gmail.com">Contact Me</a> */}
         </div>
     )
 }
