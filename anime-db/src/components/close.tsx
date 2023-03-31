498f9faaec12195829606f2ecfc109952416963f
Gamej18 <jalenbowens@ymail.com>
2023-03-31 18:40:38 -0500

    new changes

--- anime-db/src/components/close.tsx
+++ anime-db/src/components/close.tsx
@@ -4,14 +4,14 @@ import { LayoutSidebar } from "react-bootstrap-icons";
 import { X } from "react-bootstrap-icons";
 
 export default function CloseButton () {
-    const [icon, setIcon] = useState(false);
+    const [icon, setIcon] = useState(true);
     function handleclick() {
         setIcon(!icon);
         $("#menu").animate({width:'toggle'},200);
     }
     return (
         <button onClick={handleclick}>    
-          {icon ? <X size="30" color="black"/>: <LayoutSidebar size="30" color="black"/>}    
+          {icon ? <X size="30" color="black"/>: <LayoutSidebar className="layoutsidebar" size="30" color="black"/>}    
         </button>
     );
 }
