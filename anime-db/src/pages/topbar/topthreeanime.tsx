beff56e49a6dd5a02f4cc272b4c733038e41cb34
Gamej18 <jalenbowens@ymail.com>
2023-04-27 22:34:23 -0500

    new changes

--- anime-db/src/pages/topbar/topthreeanime.tsx
+++ anime-db/src/pages/topbar/topthreeanime.tsx
@@ -2,76 +2,29 @@ import $ from "jquery";
 import  juju from "../../images/Jujutsu-Kaisen-1.png";
 import vinland from "../../images/vinlandsaga.png";
 import eighty from "../../images/eighty-six.png";
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { ArrowLeftCircle, ArrowRightCircle } from "react-bootstrap-icons";
 import Image from 'next/image';
 
-interface Anime {
-    title: string;
-}
+export default function TopMainComponent() {
+    const [index, setIndex] = useState(0);
+    const img = new (Image as any)(1080, 1920);
+    img[0].src = juju;
+    img[1].src = vinland;
+    img[2].src = eighty;
 
+    function RightClick() {
+        setIndex(index + 1);
+    }
 
-export let jjk: Anime = {
-    title: "Jujutsu Kaisen",
-}
+    function LeftClick() {
+        setIndex(index - 1);
+    }
 
-export const JujuImage = () => {
     return (
-        <div>
-            <Image
-                className="juju-image"
-                id="ju"
-                src={juju}
-                alt="Jujutsu Kaisen"
-            />
-        </div>
-    )
-}
-
-// export const VinlandImage = (props: any) => {
-//     return (
-//         <div >
-//             <Image
-//                 className="vin-image"
-//                 style={{color:props}}
-//                 id="vin"
-//                 src={vinland}
-//                 alt="Vinland Saga"
-//             />
+        img[index];
+        <button onClick={RightClick}>
             
-//         </div>
-//     )
-// }
-
-
-export default function RightClick() {
-    const inputElement:any = useRef();
-
-    const RightInput = () => {
-        inputElement.current.style.zIndex = "-1";
-    }
-
-    const LeftInput = () => {
-        inputElement.current.style.zIndex = "1";
-    }
-        // requires a document??????????????????????????????????
-    return (
-        <div>
-            {/* <h1 ref={inputElement}>changed</h1> */}
-            <Image
-                className="vin-image"
-                ref={inputElement}
-                id="vin"
-                src={vinland}
-                alt="Vinland Saga"
-            />
-            <button onClick={LeftInput}>
-                <ArrowLeftCircle/>
-            </button>
-            <button onClick={RightInput}>
-                <ArrowRightCircle/>
-            </button>
-        </div>
-
+        </button>
     )
 }
